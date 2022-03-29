6944d421bea0328715fe9a18cd785a51a339278d
Isaque Lima Leite <Isaque.lima.leite.ill@gmail.com>
2022-03-29 06:30:13 +0000

    feat: add menu dropdown

--- TemRango-Prices/assets/js/script.js
+++ TemRango-Prices/assets/js/script.js
@@ -1,5 +1,10 @@
 const anchor = document.getElementsByClassName("top")
 const listItem = document.getElementsByClassName("listItem")
+const menu = document.getElementsByClassName("menu__img")
+const navMenu = document.getElementsByClassName("nav__mobile")
+const floatMenu = document.getElementsByClassName("float-menu")
+const closeMenu = document.getElementsByClassName("float-menu__close")
+const mask = document.getElementsByClassName("mask")
 
 
 anchor[0].addEventListener("click", () =>{
@@ -26,4 +31,16 @@ Array.from(listItem).forEach(item =>{
   item.addEventListener("mouseout", ()=>{
     item.classList.remove("list__selected")
   })
+})
+
+menu[0].addEventListener("click", ()=>{
+  navMenu[0].style.display = "none"
+  floatMenu[0].style.display = "block"
+  mask[0].style.display = "block"
+})
+
+closeMenu[0].addEventListener("click", ()=>{
+  navMenu[0].style.display = ""
+  floatMenu[0].style.display = ""
+  mask[0].style.display = ""
 })
